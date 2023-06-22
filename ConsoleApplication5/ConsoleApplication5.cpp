63b4c6c8438ca2ffe6f7a0e3218bd09bfcd65f13
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-22 19:45:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b, c,k = 0;
+    for (int i = 100;i <= 999;i++) {
+        a = i % 10;
+        c = i / 100;
+        b = i / 10 % 10;
+        if (a == b || a == c || b == c) {
+            k++;
+        }
+
+    }
+    cout << "Кiлькiсть чисел з однаковими цифрами: " << k;
+}
